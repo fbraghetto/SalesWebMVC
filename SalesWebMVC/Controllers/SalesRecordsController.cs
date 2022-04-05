9ccd1e6de16c8b7f215ae83e2f2e6b8c80fc6f36
fbraghetto <fbraghetto@gmail.com>
2022-04-04 23:11:45 -0300

    Implementation of Simple Search

--- SalesWebMVC/Controllers/SalesRecordsController.cs
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,30 @@ namespace SalesWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
+
+        private readonly SalesRecordService _salesRecordsService;
+
+        public SalesRecordsController(SalesRecordService salesRecordsService)
+        {
+            _salesRecordsService = salesRecordsService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+                minDate = new DateTime(2018, 1, 1);
+            if (!maxDate.HasValue)
+                maxDate = new DateTime(2018, 12, 31);
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordsService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
         public IActionResult GroupingSearch()
